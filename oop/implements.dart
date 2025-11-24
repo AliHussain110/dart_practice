d24e368fda4d736ebb62c81e22b720644e5fb250
Ali Hussain <24.alihussainimtiaz.24@gmail.com>
2025-11-24 19:59:55 +0500

    fix: replace the implements instead of extend

--- oop/implements.dart
+++ oop/implements.dart
@@ -1,28 +1,39 @@
+// The Animal class now acts as the 'interface' or 'contract'.
 class Animal {
-  String name;
-  
-  Animal(this.name);
-  
-  void makeSound() {
-    print('Some generic sound');
-  }
+  String name =
+      ''; // We must explicitly declare the property within the interface
+
+  // We define the signature that implementing classes must follow
+  void makeSound() {}
+  void wagTail() {}
 }
 
-class Dog extends Animal {
-  Dog(String name) : super(name);
-  
+// The Dog class implements the Animal contract.
+// We must now define every single member (name, makeSound, wagTail) ourselves.
+class Dog implements Animal {
+  // We MUST declare the 'name' property ourselves,
+  // we don't inherit the one from 'Animal'
+  @override
+  String name;
+
+  // We can add our own constructor specific to our implementation
+  Dog(this.name);
+
+  // We MUST provide an implementation for ALL methods in the contract
   @override
   void makeSound() {
     print('Bark!');
   }
-  
+
+  @override
   void wagTail() {
     print('Tail wagging');
   }
 }
 
-void main(){
-var dog = Dog('Rex');
-dog.makeSound(); // Bark!
-dog.wagTail();   // Tail wagging
+void main() {
+  var dog = Dog('Rex');
+  dog.makeSound(); // Bark!
+  dog.wagTail(); // Tail wagging
+  print(dog.name); // Rex
 }
